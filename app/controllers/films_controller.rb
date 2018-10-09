de7cab0f05c433a31e71ff341f7cc0bd59d08310
Andre313 <andymartinez31@live.fr>
2018-10-10 01:03:06 +0200

    Ajout d'un formulaire de modification qui a un problème de route

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -7,4 +7,16 @@ class FilmsController < ApplicationController
   	Film.create titre: params[:titre]
   	redirect_to '/films'
   end
+
+  def show
+  	@le_film = Film.find(params[:id])
+  end
+
+  def update
+  	@le_film = Film.find(params[:id])
+  	@le_film.titre = params[:titre]
+  	@le_film.save
+  	redirect_to '/films/@le_film.id'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'accueil#index'
 
   get '/films' => 'films#index'
+  get '/films/:id' => 'films#show'
+  patch 'films/:id' => 'films#update'
   post '/films' => 'films#ajout'
 
 end
