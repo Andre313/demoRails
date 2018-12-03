d9db19c00615f9a50cb0abe068690b4f4576d87b
Andre313 <andymartinez31@live.fr>
2018-12-04 00:41:08 +0100

    Ajouté formulaires de modif, supression et catégories

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_09_174344) do
+ActiveRecord::Schema.define(version: 2018_12_01_000602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+  end
 
   create_table "films", force: :cascade do |t|
     t.string "titre"
